4b961a89ec26129226a1905406fb0ee2d46bad51
Juan P <qjuanp@gmail.com>
2018-12-02 17:52:18 -0500

    🌎 Javascript events

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,2 +1,5 @@
 var form = document.getElementById('the-form')
 form.hidden = true
+
+var buyButton = document.getElementById('buy-button')
+buyButton.addEventListener('click', function() { console.log('Buying item') })
