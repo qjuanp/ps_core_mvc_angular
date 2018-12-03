9edeeee359dea267313711050ae97bb52b76cf7a
Juan P <qjuanp@gmail.com>
2018-12-02 19:52:27 -0500

    🥶 First Controller/View

---
+++ DutchTreat/Cotrollers/AppController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers {
+    public class AppController : Controller{
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
