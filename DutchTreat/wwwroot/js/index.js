76da26ab8d088c8ad5f4e93f52bae32fe4a836b1
Juan P <qjuanp@gmail.com>
2018-12-02 19:39:22 -0500

    🌎 Practical jQuery

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,12 +1,19 @@
-var form = $('#the-form')
-form.hide()
+$(document).ready(function () {
+    var form = $('#the-form')
+    form.hide()
 
-var buyButton = $('#buy-button')
-buyButton.on('click', function () {
-    console.log('Buying item')
-})
+    var buyButton = $('#buy-button')
+    buyButton.on('click', function () {
+        console.log('Buying item')
+    })
 
-var productInfo = $('.product-info li')
-productInfo.on('click', function () {
-    console.log('You clicked on ' + $(this).text())
+    var productInfo = $('.product-info li')
+    productInfo.on('click', function () {
+        console.log('You clicked on ' + $(this).text())
+    })
+
+    var $loginToggle = $('#login-toggle')
+    $loginToggle.on('click', function(){
+        $('form.popup-form').fadeToggle()
+    })
 })
