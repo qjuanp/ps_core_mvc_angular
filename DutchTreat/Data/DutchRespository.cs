80897fc5f2f75cc9773d2c5a95df367804e60ba1
Juan P <qjuanp@gmail.com>
2018-12-04 19:04:56 -0500

    😋 Added logging informatin and configuration

--- DutchTreat/Data/DutchRespository.cs
+++ DutchTreat/Data/DutchRespository.cs
@@ -3,23 +3,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using DutchTreat.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace DutchTreat.Data
 {
     class DutchRepository : IDutchRepository
     {
         private readonly DutchContext _context;
+        private readonly ILogger<DutchRepository> _logger;
 
-        public DutchRepository(DutchContext context)
+        public DutchRepository(
+            DutchContext context,
+            ILogger<DutchRepository> logger)
         {
             _context = context;
+            _logger = logger;
         }
-        public async Task<IEnumerable<Product>> GetAll() =>
-            await _context
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            _logger.LogInformation("GetAll was called");
+            return await _context
                 .Products
                 .OrderBy(p => p.Title)
                 .ToListAsync();
-
+        }
         public async Task<IEnumerable<Product>> GetByCategory(string category) =>
             await _context
                 .Products
